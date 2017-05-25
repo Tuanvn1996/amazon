1cf1e8c955f76639e38f8f3280931debd4ddad79
yeuptmh <kidqn1996@gmail.com>
2017-05-25 23:37:04 +0700

    amazon
    amazon

---
+++ src/main/java/group13/Application.java
@@ -0,0 +1,39 @@
+package group13;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import group13.storge.StorageProperties;
+import group13.storge.StorageService;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	@Bean
+	CommandLineRunner init(final StorageService storageService)
+	{
+	    return new CommandLineRunner()
+	    {
+	        @Override
+	        public void run(String... args) throws Exception
+	        {
+	            storageService.deleteAll();
+	            storageService.init();              
+	        }
+	    };
+	}
+	
+	/*CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+		};
+	}*/
+}
